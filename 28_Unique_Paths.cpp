39642c95ba1f75a7133f0a680223516f6ca2ebe7
Aditi Goyal <31741158+Adiiigo@users.noreply.github.com>
2020-07-14 22:18:49 +0530

    Update 28_Unique_Paths.cpp

--- 28_Unique_Paths.cpp
+++ 28_Unique_Paths.cpp
@@ -132,3 +132,20 @@ public:
 
 Time complexity = O(m-1,m-1)
 space complexity = O(m*n)
+
+//Approach 3 : DP
+    class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                if(i==m-1 && j==n-1) dp[i][j] = 1;
+                else if(i==m-1) dp[i][j] = dp[i][j+1];
+                else if(j==n-1) dp[i][j] = dp[i+1][j];
+                else dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};
